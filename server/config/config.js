0e5bdc496ea36d22fb0391395914bdd07e1fdf6f
Rusel <somnus085@hotmail.com>
2021-01-01 13:08:46 -0600

    Agregando la variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 //Archivo de configuración, puertos, sevillas, tokens
-//
-//
+
+
 /*PUERTO*/
 process.env.PORT= process.env.PORT || 3000;  //En el entorno local el env.PORT no existe por eso usamos el 3000 pero en el servidor(ej, Heroku) si y es el que usamos
 
